c0ef5a5625085b5defe2e3368eca20a0d5971b74
Ollie Payne <olliepayne@me.com>
2021-12-19 17:04:34 -0700

    Add: font styling

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,5 +1,16 @@
 import { Theme } from "theme-ui"
 
 export const theme: Theme = {
-  
+  fonts: {
+    heading: "Roboto",
+    body: "Nunito"
+  },
+  fontSizes: [
+    "0.875rem", // 14px
+    "1rem", // 16px
+    "1.5rem", // 24px
+    "2.25rem", // 36px
+    "3.375rem", // 54px
+    "5.063rem" // 81px
+  ],
 }
